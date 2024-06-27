49e5e39d1f7e5128a00a0c3300c41e14db4a5c07
ren12123 <s1F102401012@iniad.org>
2024-06-27 20:15:07 +0900

    オセロ盤面生成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import styles from './index.module.css';
 
 const Home = () => {
-  const [turnColor, setTurnColor] = useState(1);
   const [board, setBoard] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
@@ -13,30 +12,28 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
-
-  const clickHandler = (x: number, y: number) => {
-    console.log(x, y);
+  const [turnColor, setTurnColor] = useState(1);
+  const clickCell = (x: number, y: number) => {
     const newBoard = structuredClone(board);
-    if (board[y + 1][x] === 3 - turnColor) {
-      newBoard[y][x] = turnColor;
-      setTurnColor(3 - turnColor);
-    }
-    setBoard(newBoard);
     newBoard[y][x] = turnColor;
-    setTurnColor(turnColor === 1 ? 2 : 1);
-
     setBoard(newBoard);
+    if (turnColor === 1) {
+      setTurnColor(2);
+    } else {
+      setTurnColor(1);
+    }
   };
+
   return (
     <div className={styles.container}>
       <div className={styles.board}>
         {board.map((row, y) =>
           row.map((color, x) => (
-            <div className={styles.cell} key={`${x}-${y}`} onClick={() => clickHandler(x, y)}>
+            <div className={styles.cell} key={`${x}-${y}`} onClick={() => clickCell(x, y)}>
               {color !== 0 && (
                 <div
                   className={styles.stone}
-                  style={{ background: color === 1 ? '#000' : '#fff' }}
+                  style={{ backgroundColor: color === 1 ? '#000' : '#fff' }}
                 />
               )}
             </div>
